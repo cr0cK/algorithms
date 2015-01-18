3f7ccead4baa13904d2ce28b9720d5dcc41b0015
Alexis Mineaud <alexis.mineaud@gmail.com>
2015-01-19 00:26:33 +0100

    BFS and DSF.

---
+++ dfs/dfs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def dfs(graph, start):
+    visited, stack = set(), [start]
+
+    while stack:
+        print stack
+        vertex = stack.pop()
+        if vertex not in visited:
+            visited.add(vertex)
+            stack.extend(graph[vertex] - visited)
+    return visited
+
+
+print(dfs(graph, 'A'))
